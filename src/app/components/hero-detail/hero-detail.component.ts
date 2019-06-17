bc8af72558002084582e61f76a9b5c48f192eba9
thirty33 <joelsuarez.1101@gmail.com>
2019-06-17 10:54:53 -0500

    binding data beetween components

--- src/app/components/hero-detail/hero-detail.component.ts
+++ src/app/components/hero-detail/hero-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../../classes/hero';
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class HeroDetailComponent implements OnInit {
-
+  @Input() hero: Hero; 
   constructor() { }
 
   ngOnInit() {
